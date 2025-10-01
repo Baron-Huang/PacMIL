495ea874513c6ece68a8ebdc4f2c0396e95bb4ba
Dr. Pan Huang <59990648+Baron-Huang@users.noreply.github.com>
2025-10-02 00:20:16 +0800

    Update create_numpy.py

--- Utils/create_numpy.py
+++ Utils/create_numpy.py
@@ -1,3 +1,6 @@
+#### Author: Dr. Pan Huang
+#### Email: mrhuangpan@163.com or pan.huang@polyu.edu.hk
+#### Department: PolyU, HK
 import numpy as np
 import pandas as pd
 
@@ -6,4 +9,5 @@ if __name__ == '__main__':
     y = np.arange(0, 1, 0.02)
     write_dict = {'x':x, 'y':y}
     pd_data = pd.DataFrame(write_dict)
+
     pd_data.to_csv('E:\DHM_MIL\Results\ROC_data\Random.csv')
